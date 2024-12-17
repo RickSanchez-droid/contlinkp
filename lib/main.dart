3eda98424a53319f0802ab7308e6cf465ad0e122
deahansteyn <deahansteyn@gmail.com>
2024-12-17 13:02:41 +0000

    updated main to build

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,3 @@
-/* GitHub Actions Workflow Steps:
- * 1. cd ios
- * 2. rm -rf Pods
- * 3. rm -rf .symlinks
- * 4. rm Podfile.lock
- * 5. pod deintegrate
- * 6. pod setup
- * 7. pod install
- * 
- * Add to .github/workflows/main.yml:
- * 
- * - name: Clean iOS build
- *   working-directory: ios
- *   run: |
- *     rm -rf Pods
- *     rm -rf .symlinks
- *     rm -f Podfile.lock
- *     
- * - name: Install CocoaPods
- *   run: |
- *     sudo gem install cocoapods
- *     
- * - name: Pod Setup and Install
- *   working-directory: ios
- *   run: |
- *     pod deintegrate
- *     pod setup
- *     pod install
- */
-
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:developer' as developer;
@@ -36,16 +6,10 @@ import 'package:flutter/foundation.dart';
 void main() async {
   try {
     debugPrint("Starting app initialization");
-    
+
     // Ensure Flutter bindings are initialized
     WidgetsFlutterBinding.ensureInitialized();
     debugPrint("Flutter bindings initialized");
-    
-    // Add platform channel error handler
-    ServicesBinding.instance.defaultBinaryMessenger.setMessageHandler('crash', (message) async {
-      debugPrint('Received crash message: $message');
-      return null;
-    });
 
     runZonedGuarded(() {
       debugPrint("About to call runApp");
@@ -55,7 +19,6 @@ void main() async {
       debugPrint('Error from runZonedGuarded: $error');
       debugPrint(stack.toString());
     });
-    
   } catch (e, stack) {
     debugPrint('Error during initialization: $e');
     debugPrint(stack.toString());
@@ -92,4 +55,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
