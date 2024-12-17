f5787f09ea903c0f5691e21c86ebe870afbc371b
RickSanchez-droid <deahansteyn@gmail.com>
2024-12-17 12:40:18 +0200

    try to get error codes before flutter init

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,57 @@
+/* GitHub Actions Workflow Steps:
+ * 1. cd ios
+ * 2. rm -rf Pods
+ * 3. rm -rf .symlinks
+ * 4. rm Podfile.lock
+ * 5. pod deintegrate
+ * 6. pod setup
+ * 7. pod install
+ * 
+ * Add to .github/workflows/main.yml:
+ * 
+ * - name: Clean iOS build
+ *   working-directory: ios
+ *   run: |
+ *     rm -rf Pods
+ *     rm -rf .symlinks
+ *     rm -f Podfile.lock
+ *     
+ * - name: Install CocoaPods
+ *   run: |
+ *     sudo gem install cocoapods
+ *     
+ * - name: Pod Setup and Install
+ *   working-directory: ios
+ *   run: |
+ *     pod deintegrate
+ *     pod setup
+ *     pod install
+ */
+
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'dart:developer' as developer;
 
 void main() {
-  runZonedGuarded(() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    print('Flutter binding initialized'); // Debug print
+  try {
+    developer.log('Starting app initialization');
     
-    runApp(const MyApp());
-  }, (error, stack) {
-    print('Error: $error');
-    print('Stack trace: $stack');
-  });
+    // Ensure Flutter bindings are initialized
+    WidgetsFlutterBinding.ensureInitialized();
+    developer.log('Flutter bindings initialized');
+    
+    // Run the app inside error zone
+    runZonedGuarded(() {
+      runApp(const MyApp());
+      developer.log('App started successfully');
+    }, (error, stack) {
+      developer.log('Error during app execution',
+          error: error, stackTrace: stack);
+    });
+  } catch (e, stack) {
+    developer.log('Fatal error during initialization',
+        error: e, stackTrace: stack);
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -19,12 +60,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.blue, // Changed to make any rendering obvious
         body: Center(
           child: Text(
             'Hello World',
-            style: TextStyle(color: Colors.black),
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 32,
+            ),
           ),
         ),
       ),
